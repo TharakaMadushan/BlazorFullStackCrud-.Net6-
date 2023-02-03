d6e40debf8f50b73aa9021cce816980dea6a8b51
TharakaMadushan <madushan@perfectbss.com>
2023-02-04 00:02:59 +0530

    Add project files.

---
+++ BlazorFullStackCrud/Server/Migrations/20230202151542_Migration20230202.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorFullStackCrud.Server.Migrations
+{
+    public partial class Migration20230202 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
